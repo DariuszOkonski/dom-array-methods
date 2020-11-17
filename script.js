bca8cef5219beb95f63fcb6056eb5ce0066397f7
Dariusz <darek200180@gmail.com>
2020-11-17 21:41:08 +0000

    add calculateWealth

--- script.js
+++ script.js
@@ -48,9 +48,18 @@ function sortByRichest() {
 
 // filter Only millionaires
 function showMillionaires() {
-    data = data.filter(item => item.money > 1000000);
+    data = data.filter(user => user.money > 1000000);
     
     updateDOM();
+}
+
+// calculate wealth
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthElement = document.createElement('div');
+    wealthElement.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthElement);
 }
 
 // add new obj to data arr
@@ -83,3 +92,4 @@ addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
